ebbf331a60de23377e8259fbc21475bf7906cc0b
Meychin Lim <meychin.lm@gmail.com>
2026-05-02 16:25:12 +0700

    style: change navbar background

--- src/app/components/AppNavbar.tsx
+++ src/app/components/AppNavbar.tsx
@@ -36,14 +36,14 @@ const AppNavbar = () => {
   ];
 
   return (
-    <nav className="w-full max-w-screen-xl mx-auto py-4 shadow-2xl">
-      <div className="flex items-center justify-between">
+    <nav className="w-full py-4 shadow-xl bg-emerald-900/50 backdrop-blur-3xl font-semibold">
+      <div className="flex items-center justify-between max-w-screen-xl mx-auto">
         <Link href={"/"}>
           <Codesandbox />
         </Link>
 
         <NavigationMenu>
-          <NavigationMenuList className="flex flex-row gap-4 uppercase font-medium">
+          <NavigationMenuList className="flex flex-row gap-4 uppercase">
             {menuItems.map((item, index) => (
               <NavigationMenuItem key={index}>
                 <NavigationMenuLink href={item.href}>
